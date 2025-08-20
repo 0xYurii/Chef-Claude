cae86027d49fb408cc06f7ee381ce1eb14c19164
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-20 23:03:33 +0100

    feat: refactor App component and add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@
  * Challenge: Build the Header component in a separate file
  * and render it here in the App component
  */
+import Heder from "./Header"
 
 export default function App() {
   return (
-      <h1>Hello world!</h1>
+      <main>
+        <Heder/>
+        <h1>Hello world!</h1>
+      </main>
+      
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header(){
+    return(
+        <div className="head">
+            
+            <img className="img-chef" src="../images/chef-claude-icon.png" alt="chef icon"/>
+            <header>Chef Claude</header>
+        </div>
+    )
+}
