b03eb83fa198ccd4b577b70c6953b3960ce757be
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-21 15:15:13 +0100

    feat: refactor App component to implement a status toggle and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,18 @@
-import { useState } from "react";
+
+import {useState } from "react"
+
 export default function App() {
-  const [num,setNum]=useState(0)
-  
-  function Increase(){
-    setNum(function(){
-      return num+1
-    })
-  }
-  function Decrease(){
-    setNum(function(){
-      return num-1
-    })
-  }
+    const [status, setStatus]=useState(false);
+
+
     return (
-        <main className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
-            <div className="counter">
-                <button
-                  className="minus" 
-                  aria-label="Decrease count"
-                  onClick={Decrease}
-                >–</button>
-
-                <h2 className="count">{num}</h2>
-
-                <button
-                  className="plus" 
-                  aria-label="Increase count"
-                  onClick={Increase}
-                >+</button>
-            </div>
+        <main>
+            <h1 className="title">Do I feel like going out tonight?</h1>
+            <button
+              className="value" 
+              onClick={()=>{setStatus(!status)}}
+            
+            > {status ? "yes":"no"} </button>
         </main>
     )
 }
-
-
-
-
-
-
-// import Header from "./Header"
-// import Main from "./Main"
-
-// export default function App() {
-//   return (
-//       <>
-//         <Header/>
-//         <Main/>
-//       </>
-      
-//   )
-// }
