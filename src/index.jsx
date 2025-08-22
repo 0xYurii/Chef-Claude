37bcaa73a73c18fcf31ce62eed653755b5f6d77a
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-22 21:26:17 +0100

    feat: add password input field and update form submission handling

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-/**
- * Challenge: add another label and input for the password field
- */
+function singup(formData){
+    const email=formData.get("email")
+    const pass=formData.get("password")
+    console.log(email+pass)
+
+}
 
 function App() {
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
+      <form action={singup}>
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
         <br />
-        <label htmlFor='password'>Password:</label>
-        <input  id='password' type='password' name='password' placeholder='123456789'/>
         
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
+        <br />
         
+        <button>Submit</button>
         
       </form>
     </section>
