ceb415353bd6a570d4d256fe748bf8506cb22a58
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-24 10:41:17 +0100

    feat: implement punchline toggle functionality in Joke component

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,16 +1,21 @@
+import React from "react"
+
 export default function Joke(props) {
-    /**
-     * Challenge:
-     * - Create state `isShown` (boolean, default to `false`)
-     * - Add a button that toggles the value back and forth
-     */
+    const [isShown, setIsShown] = React.useState(false)
     
-    console.log(isShown)
+    function toggleShown() {
+        setIsShown(prevShown => !prevShown)
+    }
+    
+    /**
+     * Only display the punchline paragraph if `isShown` is true
+     */
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+            {isShown &&<p>{props.punchline}</p>}
+            <button onClick={toggleShown}>Show punchline</button>
             <hr />
         </div>
     )
-}
+} 
