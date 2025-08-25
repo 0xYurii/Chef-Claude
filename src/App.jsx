2c91ceb803f325102c6244a4d2935dea9297c712
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-25 11:03:09 +0100

    feat: scaffold drum pad interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,18 @@
 import React from "react"
-import avatar from "../images/user.png"
-import Star from "./Star"
-
+import padsData from "./pads"
 
 export default function App() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (212) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: false
-    })
+    const [pads, setPads] = React.useState(padsData)
     
-
-    function toggleFavorite() {
-        setContact(prevContact => ({
-            ...prevContact,
-            isFavorite: !prevContact.isFavorite
-        }))
-    }
+    const buttonElements = pads.map(pad => (
+        <button key={pad.id}></button>
+    ))
     
-
     return (
         <main>
-            <article className="card">
-                <img
-                    src={avatar}
-                    className="avatar"
-                    alt="User profile picture of John Doe"
-                />
-                <div className="info">
-                    <button
-                        aria-pressed={contact.isFavorite}
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        className="favorite-button"
-                        onClick={toggleFavorite}
-                    >
-                        <Star
-                            isFilled={contact.isFavorite}
-                        />
-                    </button>
-                    <h2 className="name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
-
-            </article>
+            <div className="pad-container">
+                {buttonElements}
+            </div>
         </main>
     )
 }
