5933cfbe3297fd780f2d8f3281b882fa4bd2690a
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-26 12:13:12 +0100

    feat: get back to chef claude

--- src/Count.jsx
+++
@@ -1,5 +0,0 @@
-export default function Count(props) {
-    return (
-        <h2 className="count">{props.number}</h2>
-    )
-}
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,36 @@
 import React from "react"
 
 export default function Main() {
+    /**
+     * Challenge: clean up our code!
+     * Let's make a couple new components to make things a
+     * little cleaner. (Notice: I'm not suggesting what we
+     * have now is bad or wrong. I'm mostly finding an excuse
+     * to get in some hands-on practice 🙂)
+     * 
+     * 1. Move the entire recipe <section> into its own
+     *    ClaudeRecipe component
+     * 2. Move the list of ingredients <section> into its
+     *    own IngredientsList component.
+     * 
+     * While you're considering how to structure things, consider
+     * where state is, think about if it makes sense or not to
+     * move it somewhere else, how you'll communicate between
+     * the parent/child components, etc.
+     * 
+     * The app should function as it currently does when you're
+     * done, so there will likely be some extra work to be done
+     * beyond what I've listed above.
+     */
 
     const [ingredients, setIngredients] = React.useState(
         ["all the main spices", "pasta", "ground beef", "tomato paste"]
     )
     const [recipeShown, setRecipeShown] = React.useState(false)
+    
+    function toggleRecipeShown() {
+        setRecipeShown(prevShown => !prevShown)
+    }
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -14,9 +39,6 @@ export default function Main() {
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-    }
-    function getRecipe(){
-        setRecipeShown(prev=>!prev)
     }
 
     return (
@@ -30,6 +52,7 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
+            
             {ingredients.length > 0 && <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
@@ -38,10 +61,10 @@ export default function Main() {
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
-                    <button onClick={getRecipe}>Get a recipe</button>
+                    <button onClick={toggleRecipeShown}>Get a recipe</button>
                 </div>}
             </section>}
-            {/** Paste recipeCode.md contents here */}
+            
             {recipeShown && <section>
                 <h2>Chef Claude Recommends:</h2>
                 <article className="suggested-recipe-container" aria-live="polite">
